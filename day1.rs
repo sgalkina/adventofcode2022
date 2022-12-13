a812a781f89bbe16c8e97606974bea32bdd2698a
Svetlana Kutuzova <galkina.s.a@gmail.com>
2022-12-13 20:47:10 +0100

    Day 1, part 2

--- day1.rs
+++ day1.rs
@@ -3,22 +3,21 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut best_elf: i32 = 0;
+    let mut elves = Vec::<i32>::new();
     let mut current_elf: i32 = 0;
     if let Ok(lines) = read_lines("./inputs/day1.txt") {
         for line in lines {
             if let Ok(ip) = line {
                 if "".eq(&ip) {
-                    if current_elf > best_elf {
-                        best_elf = current_elf;
-                    }
+                    elves.push(current_elf);
                     current_elf = 0;
                 } else {
                     current_elf += ip.parse::<i32>().unwrap();
                 }
             }
         }
-        println!("Best elf carries {}", best_elf);
+        elves.sort();
+        println!("Three best elves carry {}", elves.iter().rev().take(3).sum::<i32>());
     }
 }
 
