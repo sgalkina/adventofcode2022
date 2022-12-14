aefcf00d8446deb5f8ee66870c1de01b9695659c
Svetlana Kutuzova <galkina.s.a@gmail.com>
2022-12-14 22:48:01 +0100

    Day 3, part 2

--- day3.rs
+++ day3.rs
@@ -22,9 +22,50 @@ fn day3(lines: std::io::Lines<io::BufReader<File>>) {
 }
 
 
+fn day3_part2(lines: std::io::Lines<io::BufReader<File>>) {
+    let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
+    let values: Vec<i32> = (1..53).collect();
+    let scores: HashMap<char, i32> = alphabet.into_iter().zip(values.into_iter()).collect();
+
+    let mut result: i32 = 0;
+    let mut counter: i32 = 0;
+    let mut first = String::new();
+    let mut second = String::new();
+    let mut third = String::new();
+
+    for line in lines {
+        if let Ok(ip) = line {
+            if counter == 2 {
+                third = ip.clone();
+
+                let firsts: HashSet<char> = first.chars().collect();
+                let seconds: HashSet<char> = second.chars().collect();
+                let thirds: HashSet<char> = third.chars().collect();
+        
+                let diff1: HashSet<_> = firsts.intersection(&seconds).collect();
+                let diff2: HashSet<_> = thirds.intersection(&seconds).collect();
+        
+                let res = diff1.intersection(&diff2);
+                result += scores[res.collect::<Vec<&&char>>()[0]];
+
+                counter = 0;
+            } else {
+                match counter {
+                    0 => first = ip.clone(),
+                    1 => second = ip.clone(),
+                    _ => println!("Wrong"),
+                };
+                counter += 1;
+            }
+        }
+    }
+    println!("Result: {}", result);
+}
+
+
 fn main() {
     if let Ok(lines) = read_lines("./inputs/day3.txt") {
-        day3(lines);
+        day3_part2(lines);
     }
 }
 
