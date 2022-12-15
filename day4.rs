325d4df11b394936ccb625d94d9d8855c31c0310
Svetlana Kutuzova <galkina.s.a@gmail.com>
2022-12-15 20:27:55 +0100

    Day 4, part 2

--- day4.rs
+++ day4.rs
@@ -7,6 +7,10 @@ fn contains(x: &Vec<i32>, y: &Vec<i32>) -> bool {
     (x[0] <= y[0]) & (x[1] >= y[1])
 }
 
+fn overlap(x: &Vec<i32>, y: &Vec<i32>) -> bool {
+    !((x[1] < y[0]) | (y[1] < x[0]))
+}
+
 fn range(v: &str) -> Vec<i32> {
     v.split("-").map(|s| s.parse().expect("parse error")).collect::<Vec<i32>>()
 }
@@ -28,12 +32,24 @@ fn day4(lines: std::io::Lines<io::BufReader<File>>) {
 
 
 fn day4_part2(lines: std::io::Lines<io::BufReader<File>>) {
+    let mut result: i32 = 0;
+    for line in lines {
+        if let Ok(ip) = line {
+            let vals: Vec<&str> = ip.split(",").collect();
+            let range1: Vec<i32> = range(vals[0]);
+            let range2: Vec<i32> = range(vals[1]);
+            if overlap(&range1, &range2) {
+                result += 1;
+            }
+        }
+    }
+    println!("Result: {}", result);
 }
 
 
 fn main() {
     if let Ok(lines) = read_lines("./inputs/day4.txt") {
-        day4(lines);
+        day4_part2(lines);
     }
 }
 
